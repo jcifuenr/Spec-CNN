7843473189f58e890850091653cc69d1f1770da9
jcifuenr <131484652+jcifuenr@users.noreply.github.com>
2024-05-09 11:15:58 -0400

    SPEC_CNN Tensorflow version
    SPEC_CNN MODEL presented in AI and Optical Data Sciences V, SPIE OPTO, 2024, San Francisco, California, United State
    
    Mineral classification using convolutional neural networks and SWIR hyperspectral imaging
    
    https://doi.org/10.1117/12.3002101

---
+++ SPEC_CNN.py
@@ -0,0 +1,62 @@
+import warnings
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.layers import Input, Concatenate
+from tensorflow.keras.models import Model
+from tensorflow.keras.initializers import RandomNormal
+from tensorflow.keras.callbacks import ModelCheckpoint
+from sklearn.model_selection import KFold, StratifiedKFold
+from tensorflow.keras.utils import set_random_seed
+from tensorflow.keras.callbacks import ModelCheckpoint
+
+set_random_seed(42)
+warnings.filterwarnings('ignore')
+initializer = tf.keras.initializers.RandomNormal(mean=0.0, stddev=0.5, seed=42)
+
+wave_shape = 256
+Input_shape_scpec = (256,1)
+num_classes = 9
+
+def SPEC_CNN():
+
+    input_shape = Input_shape_scpec
+    inputs = Input(shape=input_shape)
+    x = Dense(wave_shape,kernel_regularizer=tf.keras.regularizers.L1(0.01), kernel_initializer=initializer)(inputs)
+    # Block 1
+    x = Conv1D(32, 3, strides=2, padding='same', activation='relu')(inputs)
+    x = Conv1D(32, 3, strides=1, padding='same', activation='relu')(x)
+    x = Conv1D(32, 3, strides=1, padding='same', activation='relu')(x)
+    skip1 = MaxPooling1D(pool_size=2, strides=1)(x)
+    x = Dropout(0.25)(skip1)
+
+    # Block 2
+    x = Conv1D(64, 3, strides=2, padding='same', activation='relu')(x)
+    x = Conv1D(64, 3, strides=1, padding='same', activation='relu')(x)
+    x = Conv1D(64, 3, strides=1, padding='same', activation='relu')(x)
+    skip2 = MaxPooling1D(pool_size=2, strides=1)(x)
+    x = Dropout(0.25)(skip2)
+
+    # Block 3
+    x = Conv1D(128, 3, strides=2, padding='same', activation='relu')(x)
+    x = Conv1D(128, 3, strides=1, padding='same', activation='relu')(x)
+    x = Conv1D(128, 3, strides=1, padding='same', activation='relu')(x)
+    skip3 = MaxPooling1D(pool_size=2, strides=1)(x)
+    x = Dropout(0.25)(skip3)
+
+    # Block 4
+    x = Conv1D(256, 3, strides=2, padding='same', activation='relu')(x)
+    x = Conv1D(256, 3, strides=1, padding='same', activation='relu')(x)
+    x = Conv1D(256, 3, strides=1, padding='same', activation='relu')(x)
+    x = MaxPooling1D(pool_size=2, strides=1)(x)
+    x = Dropout(0.25)(x)
+
+    x = Flatten()(x)
+    x = Concatenate()([x, Flatten()(skip1)])
+    x = Concatenate()([x, Flatten()(skip2)])
+    x = Concatenate()([x, Flatten()(skip3)])
+
+    x = Dense(512, activation='linear')(x)
+    outputs = Dense(num_classes, activation='softmax')(x)
+
+    model = Model(inputs=inputs, outputs=outputs)
+    return model
